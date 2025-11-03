3e9938a10e887266392feb7670aac882bfa540e6
BartoszewskiA <bartoszewski.a@outlook.com>
2025-11-03 13:43:16 +0100

    03.11.25 (b)

--- lab05p01-atlas/app/src/main/java/com/example/lab05p01_atlas/MainActivity.java
+++ lab05p01-atlas/app/src/main/java/com/example/lab05p01_atlas/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab05p01_atlas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] rysunki = {R.drawable.prawdziwek,
+    R.drawable.podgrzybek,
+    R.drawable.muchomor,
+    R.drawable.kania};
+
+    String[] nazwy;
+    String[] opisy;
+    int pozycja =0;
+
+    TextView tv_nazwa, tv_opis;
+    ImageView ramka;
+    Button btn_p, btn_n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,41 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        nazwy = getResources().getStringArray(R.array.nazwy);
+        opisy = getResources().getStringArray(R.array.opisy);
+        znajdzUchwyty();
+
+        tv_nazwa.setText(nazwy[pozycja]);
+        tv_opis.setText(opisy[pozycja]);
+        ramka.setImageResource(rysunki[pozycja]);
+
+        View.OnClickListener sluchacz = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int Id = v.getId();
+                if (Id == R.id.button01) {
+                    pozycja++;
+                    if (pozycja >= rysunki.length)
+                        pozycja = 0;
+                } else if (Id == R.id.button02) {
+                        pozycja--;
+                        if (pozycja < 0)
+                            pozycja = rysunki.length - 1;
+                }
+                tv_nazwa.setText(nazwy[pozycja]);
+                tv_opis.setText(opisy[pozycja]);
+                ramka.setImageResource(rysunki[pozycja]);
+            }
+        };
+        btn_n.setOnClickListener(sluchacz);
+        btn_p.setOnClickListener(sluchacz);
+    }
+
+    private void znajdzUchwyty() {
+        tv_nazwa = findViewById(R.id.textView02);
+        tv_opis = findViewById(R.id.textView03);
+        ramka = findViewById(R.id.imageView01);
+        btn_p = findViewById(R.id.button01);
+        btn_n = findViewById(R.id.button02);
     }
 }
